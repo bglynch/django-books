382e62bfabb00710ccb321d9060ea5471af35d1e
Brian Lynch <bglynch17@gmail.com>
2018-06-13 15:47:52 +0000

    added python for login and register

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+# accounts url.py
+
+from django.conf.urls import url
+from accounts.views import login, register
+
+
+urlpatterns = [
+    url(r'^login', login, name='login'),
+    url(r'^register$', register, name='register'),
+]
