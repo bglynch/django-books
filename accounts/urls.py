b396847ba511137e2bbf5b8c49f43f9a9f0efcf4
Brian Lynch <bglynch17@gmail.com>
2018-06-14 08:49:46 +0000

    functionality to login, register, profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 # accounts url.py
 
 from django.conf.urls import url
-from accounts.views import login, register
+from accounts.views import login, register, logout, profile
 
 
 urlpatterns = [
     url(r'^login', login, name='login'),
     url(r'^register$', register, name='register'),
+    url(r'^logout$', logout, name='logout'),
+    url(r'^profile', profile, name='profile'),
 ]
