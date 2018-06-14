7b731feb2175927415177a97aa502ea25fae0eef
Brian Lynch <bglynch17@gmail.com>
2018-06-14 15:57:42 +0000

    Displayed books according to owner

--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
 # books/model.py
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
@@ -8,7 +9,8 @@ class Book(models.Model):
     author  = models.CharField(max_length=254, default='')
     isbn    = models.CharField(max_length=254, default='')
     notes   = models.TextField()
-    image = models.ImageField(upload_to='images', default='images/default.png') 
+    image = models.ImageField(upload_to='images', default='images/default.png')
+    owner = models.ForeignKey(User, related_name='books', null=False)
     
     def __str__(self):
         return self.name
--- books/views.py
+++ books/views.py
@@ -5,14 +5,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def get_index(request):
-    books = Book.objects.all()
+    if request.user.is_authenticated:
+        books = Book.objects.filter(owner=request.user)
+    else:
+        books = []
     return render(request, 'index.html', {'books': books})
 
 def add_book(request):
     if request.method == "POST":
         add_book_form = AddBookForm(request.POST, request.FILES)
         if add_book_form.is_valid():
-            add_book_form.save()
+            book = add_book_form.save(commit=False)
+            book.owner = request.user
+            book.save()
         return redirect('/')
     else:
         add_book_form = AddBookForm()
